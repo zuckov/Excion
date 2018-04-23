cba2ee343beb311535431ac2c523e969b493ad3c
zuckov <kenan.armyansyah@gmail.com>
2018-04-23 19:24:12 +0700

    Initial commit

---
+++ application/controllers/main.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ */
+ defined('BASEPATH') OR exit('No direct script access allowed');
+class Main extends CI_Controller {
+
+
+    public function index()
+    {
+      $this->load->view('view');
+    }
+
+    public function view($page='home') {
+      if (!file_exists(APPPATH.'views/pages/'.$page.'.php')) {
+        # kalo ga nemu
+        show_404();
+      }
+      $data['title'] =ucfirst($page); //biar huruf besar awalannya
+
+      $this->load->view('templates/header', $data);
+      $this->load->view('pages/'.$page, $data);
+      $this->load->view('templates/header', $data);
+    }
+
+
+    public function fgetcsv(){
+      echo "halo";
+
+    }
+
+}
+
+
+?>
---
+++ application/views/pages/home.php
@@ -0,0 +1,8 @@
+<?php echo "ini home"; ?>
+<br>
+<?php //echo base_url; ?>
+<br>
+
+coba <a href="index.php/main/fgetcsv">fgetcsv</a>
+<br>
+bantuan klik <a href="user_guide/">di sini</a>
