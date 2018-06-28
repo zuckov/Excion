331f775df10b0a2a7fd6ae89606790621b950f13
zuckov <kenan.armyansyah@gmail.com>
2018-06-28 15:40:57 +0700

    Ekperimen tampilaan lanjut
    Coba bangun tampilan web pake suppablog

---
+++ application/controllers/tampilan.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+  public function index(){
+    $this->load->view('templates/supablog/index');
+  }
+
+}
+
+
+?>
