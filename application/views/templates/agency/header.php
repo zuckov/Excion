4a7fe67628bcead09219564ef4f158565fc7ce53
zuckov <kenan.armyansyah@live.com>
2018-08-01 00:42:16 +0700

    nambah glyphicon

--- application/views/templates/agency/header.php
+++ application/views/templates/agency/header.php
@@ -32,6 +32,19 @@
 	<link rel="stylesheet" href="<?php echo base_url() ?>content/creative-agency/css/font-awesome.min.css">
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>content/creative-agency/css/style.css" />
 
+
+	<style>
+  .modal-header, h4, .close {
+      background-color: #5cb85c;
+      color:white !important;
+      text-align: center;
+      font-size: 30px;
+  }
+  .modal-footer {
+      background-color: #f9f9f9;
+  }
+  </style>
+
 	<!-- Dropzone CSS -->
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>content/multiUpload/dropzone.css" />
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>content/multiUpload/basic.css" />
