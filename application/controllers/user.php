03ebf7316193156b1bb8e8cde7d33a1cae048f1a
zuckov <kenan.armyansyah@live.com>
2018-08-10 22:58:29 +0700

    coba profile?

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -20,9 +20,7 @@ class User extends CI_Controller {
 	 */
 	function __construct(){
  		parent::__construct();
-		if($this->session->userdata('lvl') != 1){
-			redirect(base_url());
-		}
+
  		$this->load->model('m_user');
 		$this->load->model('m_region');
 
@@ -31,6 +29,9 @@ class User extends CI_Controller {
  	}
 	public function index()
 	{
+		if($this->session->userdata('lvl') != 1){
+			redirect(base_url());
+		}
 		//$data['region'] = $this->m_user->tampil_data()->result();
     //$cek =$this->m_user->tampil_data()->num_rows();
     //$data['user'] = $this->m_user->tampil_data();
@@ -61,6 +62,9 @@ class User extends CI_Controller {
 	}
 
 	function tambah_aksi(){
+		if($this->session->userdata('lvl') != 1){
+			redirect(base_url());
+		}
 		$this->form_validation->set_rules('username','username','required|trim');
 		$this->form_validation->set_rules('password','password','required|trim');
 		$this->form_validation->set_rules('nama','nama','required|trim');
@@ -96,18 +100,27 @@ class User extends CI_Controller {
 	}
 
   function hapus($id){
+		if($this->session->userdata('lvl') != 1){
+			redirect(base_url());
+		}
 		$where = array('id' => $id);
 		$this->m_user->delete_data($where,'user');
 		redirect('user/index');
 	}
 
   function edit($id){ //ini sukses
+		if($this->session->userdata('lvl') != 1){
+			redirect(base_url());
+		}
 		$where = array('id' => $id);
 		$data['user'] = $this->m_user->edit_data($where,'user')->result();
 		$this->load->view('user/v_edit',$data);
 	}
 
   function update($id){ //ini kenapa?
+		if($this->session->userdata('lvl') != 1){
+			redirect(base_url());
+		}
 		//$id = $this->input->post('id');
 		$pass = $this->input->post('password');
 		$username = $this->input->post('username');
@@ -132,5 +145,32 @@ class User extends CI_Controller {
 		$this->m_user->update_data($where,$data,'user');
 		$this->session->set_flashdata('pesan2','Data input berhasil');
 		redirect('user');
+	}
+
+	function update_profile($id){
+		$username = $this->input->post('username');
+		$pass = $this->input->post('password');
+		$password = md5($pass);
+		$nama = $this->input->post('nama');
+		$email = $this->input->post('email');
+		$lvl = $this->input->post('lvl');
+		$reg = $this->input->post('region');
+
+		$data = array(
+			'username' => $username,
+			'password' => $password,
+			'nama' => $nama,
+			'email' => $email,
+			'level_user' => $lvl,
+			'id_region' => $reg,
+		);
+		$where = array(
+			'id' => $id
+		);
+
+		$this->m_user->update_data($where,$data,'user');
+		$this->session->set_flashdata('pesan2','Data input berhasil');
+		redirect('user');
+
 	}
 }
